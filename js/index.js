2124d64492598f6acce3f20e662e5d90ab13fbc8
Svitlana Melashenko <svetapogranichna@gmail.com>
2017-11-10 15:25:48 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+const n = Number(prompt('Type number of Fibonacci'));   
+function fib(n) {
+ if( n <= 2){
+   return 1;
+ }
+  let a = 1;
+  let b = 1;
+  let c= 0;
+  for (var i = 3; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
+}
+
+document.write( fib(n) ); 
